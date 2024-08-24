9b6b06cdfca45c04a0a1fa9eb5cfb5cbef0f2aa1
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-08-24 11:45:45 -0700

    feat: Update article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,20 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article Updated Successfully"
+      redirect_to @article
+    else
+      render "edit", status: :unprocessable_entity
+    end
+  end
+
   def show
     @article = Article.find(params[:id])
   end
