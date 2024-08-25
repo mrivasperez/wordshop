9300cb0a871d0b22437c313830fa16195c8ac227
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-08-25 13:59:00 -0700

    task: Associate articles with users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 class User < ApplicationRecord
+  has_many :articles
   validates :username, presence: true,
                        uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
   validates :email, presence: true,
