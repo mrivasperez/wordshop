3546b25fbdac4e92e82d4f9c1bf41dab43461c03
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-08-25 13:52:14 -0700

    task: Add username and email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
 class User < ApplicationRecord
+  validates :username, presence: true,
+                       uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    length: { maximum: 105 },
+                    format: { with: VALID_EMAIL_REGEX }
 end
