07c167b86b4eede6a96d596206af18281657b2db
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-09-04 10:16:14 -0700

    feat: Paginate user index, articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
-    @articles = Article.paginate(page: params[:page], per_page: 2)
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
   def new
