612f1044afacc56fac8547e567d55dca6adfc6f4
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-09-06 10:16:13 -0700

    feat: Logout user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
       flash[:notice] = "Welcome back!"
       redirect_to user
     else
@@ -14,5 +15,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "See ya later!"
+    redirect_to root_path
   end
 end
