aa11668dcd35e0502948733c533c95751bf55ccc
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-09-06 10:08:21 -0700

    feat: User can sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash[:notice] = "Welcome back!"
+      redirect_to user
+    else
+      flash.now[:alert] = "Hmm, it looks like something's not quite right. Let's try that again!"
+      render "new", status: :unprocessabe_entity
+    end
   end
 
   def destroy
